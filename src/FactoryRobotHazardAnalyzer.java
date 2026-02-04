a1a8549c590598a4f3b4c8e48e8d43a983ef2828
ab4041 <138356134+ab4041@users.noreply.github.com>
2026-02-04 11:31:34 +0530

    [Anirudh] Added: UC2: accept robot hazard inputs from user

--- src/FactoryRobotHazardAnalyzer.java
+++ src/FactoryRobotHazardAnalyzer.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class FactoryRobotHazardAnalyzer {
     public static void main(String[] args) {
-        System.out.println("Factory Robot Hazard Analyzer");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter Arm Precision (0.0 - 1.0):");
+        double armPrecision = sc.nextDouble();
+
+        System.out.println("Enter Worker Density (1 - 20):");
+        int workerDensity = sc.nextInt();
+        sc.nextLine();
+
+        System.out.println("Enter Machinery State (Worn/Faulty/Critical):");
+        String machineryState = sc.nextLine();
+
+        System.out.println("Arm Precision: " + armPrecision);
+        System.out.println("Worker Density: " + workerDensity);
+        System.out.println("Machinery State: " + machineryState);
+
+        sc.close();
     }
 }
